0a6e305fdc16bbe245e981c58af667c77dd41dc0
Phelipe Galiotti <pgaliotti@esapiens.com.br>
2025-07-11 10:38:23 -0300

    git commit -m "bkp do terminal setup personalizado"

---
+++ README.md
@@ -0,0 +1,20 @@
+# Setup rápido
+
+Backup e setup rápido das minhas configurações de terminal (Zsh, Powerlevel10k, Oh My Zsh, etc).
+
+## Instalação rápida
+
+```bash
+git clone https://github.com/seuusuario/dotfiles.git
+cd terminal-setup
+./setup.sh
+```
+
+## O que está incluso?
+- .zshrc customizado
+- Powerlevel10k
+- Aliases e funções pessoais
+- Plugin Oh My Zsh
+
+## Recomendações
+- Instale o Zsh e o Git antes de rodar o setup
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cp ./zshrc ~/.zshrc
+cp ./p10k.zsh ~/.p10k.zsh
+
+#Instale o Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+#Instalar zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#Instalar zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+echo "Pronto! Agora é só abrir o terminal."
